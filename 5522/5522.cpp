0fb75f2eb84f065079a0e92a130feb05ccebee6b
KimSeonGyu <sklationd@gmail.com>
2020-03-29 14:23:26 +0900

    5522

---
+++ 5522/5522.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int i;
+    int sum = 0;
+    for(i=0;i<5;i++){
+        int temp;
+        cin >> temp;
+        sum += temp;
+    }
+    cout << sum;
+}

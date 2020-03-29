de811ea9e4947a35539239debbf428a0b68f57fc
KimSeonGyu <sklationd@gmail.com>
2020-03-29 13:24:03 +0900

    3003

---
+++ 3003/3003.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int K,Q,R,B,N,P;
+    cin >> K >> Q >> R >> B >> N >> P;
+    cout << 1-K << " " << 1-Q << " " << 2-R << " " << 2-B << " " << 2-N << " " << 8-P;
+}

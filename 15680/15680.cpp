528c18cc0ad53a08e73097ad3b59362bf8be6674
KimSeonGyu <sklationd@gmail.com>
2020-03-31 01:39:29 +0900

    15680

---
+++ 15680/15680.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // for fast io 
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,i;
+    cin >> N;
+    if(N==0) cout << "YONSEI";
+    else cout << "Leading the Way to the Future";
+}

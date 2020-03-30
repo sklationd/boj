a46e279e7a2b8c5d529470a6aec3ade056150fe5
KimSeonGyu <sklationd@gmail.com>
2020-03-31 00:59:52 +0900

    17863

---
+++ 17863/17863.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // for fast io 
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,i;
+    cin >> N;
+    if(N/10000 == 555) cout << "YES";
+    else cout << "NO";
+}

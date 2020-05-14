2cfc13b0f0bd29fc2ff8dd2af7feb0a2910b3742
KimSeonGyu <sklationd@gmail.com>
2020-05-14 21:00:06 +0900

    9663

---
+++ 9663/9663.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // for fast io 
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,i;
+    cin >> N;
+    for(i=0;i<N;i++){
+        
+    }
+}

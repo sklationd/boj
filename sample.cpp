9e65036deeccce4626833e8f6152ec975f1ad7c8
KimSeongyu <skkim@wom.ai>
2020-05-27 22:34:45 +0900

    Edit sample cpp

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
 using namespace std;
+
+typedef long long int ll;
+
 int main(){
     // for fast io 
     ios_base :: sync_with_stdio(false);

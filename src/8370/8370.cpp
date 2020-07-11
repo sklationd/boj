0f93fdd29cf560fd1c796dc1a930bfb03cb8af95
김선규 <skla@gimseongyuui-MacBookPro.local>
2020-07-11 16:11:28 +0900

    [Misc] 8370

---
+++ src/8370/8370.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // for fast io 
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << a*b+c*d;
+}

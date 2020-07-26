f32f96f0c3257e2dec4f7a3cc3463c7c4ac3cef3
sklationd <sklationd@gmail.com>
2020-07-26 22:52:36 +0900

    [Tree/UCPC2020] 19535

--- src/19535/19535.cpp
+++ src/19535/19535.cpp
@@ -12,14 +12,14 @@ int main(){
 
     int N,i;
     cin >> N;
-    vector<vector<int>> V(N+1);
     vector<pair<int,int>> E(N-1);
+    vector<int> d_count(N+1,0);
 
     for(i=0;i<N-1;i++){
         int v1,v2;
         cin >> v1 >> v2;
-        V[v1].push_back(v2);
-        V[v2].push_back(v1);
+        d_count[v1]++;
+        d_count[v2]++;
         E[i].first = v1;
         E[i].second = v2;
     }
@@ -28,9 +28,9 @@ int main(){
     ll G = 0;
 
     for(i=1;i<=N;i++){
-        if(V[i].size() >= 3) {
-            ll s = V[i].size();
-            G += s*(s-1)*(s-2) / 6 ;
+        if(d_count[i] >= 3) {
+            ll s = d_count[i];
+            G += s*(s-1)*(s-2)/6;
         }
     }
 
@@ -38,15 +38,17 @@ int main(){
         int v1,v2;
         v1 = E[i].first;
         v2 = E[i].second;
-        ll l = V[v1].size() - 1;
-        ll r = V[v2].size() - 1;
+        ll l = d_count[v1] - 1;
+        ll r = d_count[v2] - 1;
         D += l * r;
     }
 
     if(D > 3*G)
-        cout << "D" << endl;
+        cout << "D\n";
     else if(D < 3*G) 
-        cout << "G" << endl;
+        cout << "G\n";
     else
-        cout << "DUDUDUNGA" << endl;    
+        cout << "DUDUDUNGA\n";    
+    
+    return 0;
 }

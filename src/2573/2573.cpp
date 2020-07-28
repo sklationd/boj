09b6137614fd46649a3f26ab2339d1e5dd3762fc
sklationd <sklationd@gmail.com>
2020-07-28 22:59:49 +0900

    [BFS] 2573

--- src/2573/2573.cpp
+++ src/2573/2573.cpp
@@ -7,34 +7,15 @@ using namespace std;
 
 int N,M,C=0;
 int map[301][301] = {0,};
-int mark[301][301] = {0,};    
-
-// Fast IO
-char buf[1 << 17];
-char read() {
-	static int idx = 1 << 17;
-	if (idx == 1 << 17) {
-		fread(buf, 1, 1 << 17, stdin);
-		idx = 0;
-	}
-	return buf[idx++];
-}
-int readInt() {
-	int t, r = read() & 15;
-	while ((t = read()) & 16) r = r * 10 + (t & 15);
-	return r;
-}
-
-// Sea?
-int sea(int i, int j){
-    if(map[i][j]==0)
-        return 1;
-    else
-        return 0;    
-}
+int mark[301][301] = {true};
+pair<int, int> iceberg;
 
 int meltCount(int i, int j){
-    int seaCount = sea(i-1,j) + sea(i+1,j) + sea(i,j-1) + sea(i,j+1);
+    int seaCount = 0;
+    if(!map[i-1][j]) seaCount++;
+    if(!map[i+1][j]) seaCount++;
+    if(!map[i][j-1]) seaCount++;
+    if(!map[i][j+1]) seaCount++;
     return seaCount;
 }
 
@@ -51,22 +32,23 @@ int melt(queue<tuple<int, int, int>> &count){
     return melted;
 }
 
-int bfs(queue<tuple<int, int, int>> &meltC){
-    bool mark[301][301] = {false};
+int bfs(queue<tuple<int, int, int>> &meltC, int t){
     queue<pair<int, int>> Q;
     pair<int, int> P;
 
-    for(int i=1;i<N-1;i++){
-        for(int j=1;j<M-1;j++){
-            if(map[i][j]!=0){
-                P.first = i;
-                P.second = j;
+    if(map[iceberg.first][iceberg.second]==0){
+        for(int i=1;i<N-1;i++){
+            for(int j=1;j<M-1;j++){
+                if(map[i][j]!=0){
+                    iceberg.first = i;
+                    iceberg.second = j;
+                }
             }
         }
     }
 
-    Q.push(P);
-    mark[P.first][P.second] = true;
+    Q.push(iceberg);
+    mark[iceberg.first][iceberg.second]++;
 
     int count = 0;
     while(!Q.empty()){
@@ -78,33 +60,24 @@ int bfs(queue<tuple<int, int, int>> &meltC){
 
         meltC.push(make_tuple(r,c,meltCount(r, c)));
 
-        if(!mark[r-1][c] && map[r-1][c]!=0) {
+        if(mark[r-1][c]<t+1 && map[r-1][c]!=0) {
             Q.push(make_pair(r-1,c));
-            mark[r-1][c] = true;
+            mark[r-1][c]++;
         }
-        if(!mark[r+1][c] && map[r+1][c]!=0) {
-            mark[r+1][c] = true;
+        if(mark[r+1][c]<t+1 && map[r+1][c]!=0) {
+            mark[r+1][c]++;
             Q.push(make_pair(r+1,c));
         }
-        if(!mark[r][c-1] && map[r][c-1]!=0) {
-            mark[r][c-1] = true;
+        if(mark[r][c-1]<t+1 && map[r][c-1]!=0) {
+            mark[r][c-1]++;
             Q.push(make_pair(r,c-1));
         } 
-        if(!mark[r][c+1] && map[r][c+1]!=0) {
-            mark[r][c+1] = true;
+        if(mark[r][c+1]<t+1 && map[r][c+1]!=0) {
+            mark[r][c+1]++;
             Q.push(make_pair(r,c+1));
         }
     }
     return count;
-}
-
-void print_map(){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            cout << map[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
 
 int main(){
@@ -113,24 +86,24 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    N = readInt();
-    M = readInt();
-
-    vector<pair<int,int>> iceberg;
-
+    cin >> N >> M;
+    
     for(int i=0;i<N;i++){
         for(int j=0;j<M;j++){
-            map[i][j] = readInt();
-            if(map[i][j]) C++;
+            cin >> map[i][j];
+            if(map[i][j]) {
+                C++;
+            }
         }
     }
 
+    iceberg = make_pair(1,1);
     queue<tuple<int, int, int>> count;
     int time = 0;
 
     while(C!=0){
 
-        if(C!=0 && bfs(count) < C){
+        if(C!=0 && bfs(count,time) < C){
             break;
         }
 

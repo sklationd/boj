d74d8eb92acfcf73104e3070a035c62523f9a5d2
sklationd <sklationd@gmail.com>
2020-08-02 12:51:47 +0900

    [BigNumber] 15353

--- src/15353/15353.cpp
+++ src/15353/15353.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <string>
 #include "string.h"
 
 using namespace std;
 
-typedef long long int ll;
-
-int add(string &a, string &b, string &r){
-    int la = a.size();
-    int lb = b.size();
+int add(char *a, char *b, char *r){
+    int la = strlen(a);
+    int lb = strlen(b);
 
     int i;
     int c = 0;
+
     for(i=1;i<=la+1;i++){
         int T = c;
-        if(la - i >= 0)
+        if(la - i >= 0){
             T += (a[la-i] - '0');
-        if(lb - i >= 0)
+        }
+        if(lb - i >= 0){
             T += (b[lb - i] - '0');
+        }
         c = T/10;
-        cout << c << endl;
         T %= 10;
         r[i-1] = (char)(T+'0');
     }
@@ -32,13 +31,14 @@ int add(string &a, string &b, string &r){
 }
 
 int main(){
-    string a;
-    string b;
-    string r;
+    char a[10001];
+    char b[10001];
+    char r[10001];
 
     cin >> a >> b;
+
     int l;
-    if(a.size() > b.size())
+    if(strlen(a) > strlen(b))
         l = add(a,b,r);
     else
         l = add(b,a,r);    

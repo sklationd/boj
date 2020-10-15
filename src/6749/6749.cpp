89a962e6fe19930c779a95a7144958c0e7f9439a
sklationd <sklationd@gmail.com>
2020-10-15 13:29:53 +0900

    [IO] 6749

---
+++ src/6749/6749.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+int main(){
+    // for fast io 
+    fastio;
+	int a,b;
+	cin >> a >> b;
+	cout << 2*b - a << endl;
+}
